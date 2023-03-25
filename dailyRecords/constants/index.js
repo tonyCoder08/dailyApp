58c6fed66ffc04d30f4b568c943c23255e663ca1
tonyCoder <127786984+tonyCoder08@users.noreply.github.com>
2023-03-25 12:23:07 +0000

    site dummy data

--- dailyRecords/constants/index.js
+++ dailyRecords/constants/index.js
@@ -1,44 +1,67 @@
 export const palette = {
-    primary:"#fff",
-    secondary:"#F8F9FA",
-    textColor:"#11181C",
-    borderColor:"#D7DBDF",
-    success:"#70FF98",
-    successTextColor:"#419E5B",
-    blue:"#70D4FF",
-    blueTextColor:"#4291B2",
-    yellow:"#FFE070",
-    yellowTextColor:"#FF7070"
+    primary: "#fff",
+    secondary: "#F8F9FA",
+    textColor: "#11181C",
+    borderColor: "#D7DBDF",
+    success: "#70FF98",
+    successTextColor: "#419E5B",
+    blue: "#70D4FF",
+    blueTextColor: "#4291B2",
+    yellow: "#FFE070",
+    yellowTextColor: "#FF7070"
 }
 
 export const design = {
-    paddingSize:10,
-    borderRadius:11,
-    borderRadiusProfile:9
+    paddingSize: 10,
+    borderRadius: 11,
+    borderRadiusProfile: 9
 }
 
 
 
-export const employees =[
+export const employees = [
     {
-        id:1,
-        name:"Santosh Kumar",
-        profile:"https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8bWFuJTIwcG9ydHJhaXR8ZW58MHx8MHx8&w=1000&q=80",
-        currently:"Kalyaninagar"
-    
+        id: 1,
+        name: "Santosh Kumar",
+        profile: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8bWFuJTIwcG9ydHJhaXR8ZW58MHx8MHx8&w=1000&q=80",
+        currently: "Kalyaninagar"
+
     },
     {
-        id:2,
-        name:"Ghevaram Suthar",
-        profile:"https://images.unsplash.com/photo-1566492031773-4f4e44671857?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fHBvcnRyYWl0JTIwbWFufGVufDB8fDB8fA%3D%3D&w=1000&q=80",
-        currently:"Kalyaninagar"
-    
+        id: 2,
+        name: "Ghevaram Suthar",
+        profile: "https://images.unsplash.com/photo-1566492031773-4f4e44671857?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fHBvcnRyYWl0JTIwbWFufGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+        currently: "Kalyaninagar"
+
     },
     {
-        id:3,
-        name:"Sawai suthar",
-        profile:"https://img.freepik.com/free-photo/latin-man-smiling-mockup-psd-cheerful-expression-closeup-portrai_53876-145665.jpg",
-        currently:"Kalyaninagar"
-    
+        id: 3,
+        name: "Sawai suthar",
+        profile: "https://img.freepik.com/free-photo/latin-man-smiling-mockup-psd-cheerful-expression-closeup-portrai_53876-145665.jpg",
+        currently: "Kalyaninagar"
+
     },
+]
+
+export const sites = [
+    {
+        id: 1,
+        client_name: "ashok sharma",
+        address: "unknown street pune satara road ambegaon kh jambhulwadi katraj",
+        state:"Not Started"
+    },
+    {
+        id: 3,
+        client_name: "mahendra suthar",
+        address: "unknown street pune satara road ambegaon kh jambhulwadi katraj",
+        state:"Done"
+    },
+    {
+        id: 2,
+        client_name: "kunal malhotra",
+        address: "unknown street pune satara road ambegaon kh jambhulwadi katraj",
+        state:"Working"
+    },
+
+
 ]
