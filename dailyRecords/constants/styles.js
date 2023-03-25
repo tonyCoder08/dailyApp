de2e105242ad550253199a9dcfd7f456dfde7be3
tonyCoder <127786984+tonyCoder08@users.noreply.github.com>
2023-03-25 18:14:47 +0000

    global styles

---
+++ dailyRecords/constants/styles.js
@@ -0,0 +1,41 @@
+import { StyleSheet } from "react-native";
+import { design, palette } from ".";
+
+export const globalstyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: palette.secondary,
+        width: "100%"
+    },
+    headerContainer: {
+        width: "100%",
+        // height:70,
+        borderBottomColor: palette.borderColor,
+        borderBottomWidth: 1,
+        backgroundColor: palette.primary,
+        padding: design.paddingSize,
+        flexDirection: "row",
+        alignItems: "center"
+    },
+    headerTitle: {
+        fontSize: 20,
+        fontFamily: "Inter_600SemiBold"
+    },
+    box: {
+        backgroundColor: palette.primary,
+        borderWidth: 1,
+        borderColor: palette.borderColor,
+        width: "100%",
+        borderRadius: design.borderRadiusProfile,
+        padding: design.paddingSize,
+        marginBottom: 10
+    }, Section: {
+        padding: design.paddingSize,
+        flex: 1
+    },
+    ownerProfile: {
+        resizeMode: "contain",
+        borderRadius: design.borderRadiusProfile,
+        marginRight: 10
+    },
+})
