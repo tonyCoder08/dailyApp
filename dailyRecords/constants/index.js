961b53861a4ca1c55347a5fec523cc1690c55762
tonyCoder <127786984+tonyCoder08@users.noreply.github.com>
2023-03-25 08:02:37 +0000

    home page add

--- dailyRecords/constants/index.js
+++ dailyRecords/constants/index.js
@@ -4,10 +4,15 @@ export const palette = {
     textColor:"#11181C",
     borderColor:"#D7DBDF",
     success:"#70FF98",
-    successTextColor:"#419E5B"
+    successTextColor:"#419E5B",
+    blue:"#70D4FF",
+    blueTextColor:"#4291B2",
+    yellow:"#FFE070",
+    yellowTextColor:"#FF7070"
 }
 
 export const design = {
     paddingSize:10,
     borderRadius:11,
+    borderRadiusProfile:9
 }
--- dailyRecords/screens/Home.js
+++ dailyRecords/screens/Home.js
@@ -1,3 +1,4 @@
+import { Platform, TouchableOpacity } from "react-native";
 import { View, Text, StyleSheet, Image, ImageBackground } from "react-native";
 import { design, palette } from "../constants";
 
@@ -5,8 +6,8 @@ const Home = () => {
     return (
         <View style={styles.container}>
             <View style={styles.headerContainer}>
-                    <Image style={styles.ownerProfile} source={{ width: 58, height: 58, uri: 'https://fixthephoto.com/blog/images/uikit_slider/male-photo-edited-by-fixthephoto-service_1649799173.jpg' }}>
-                    </Image>
+                <Image style={styles.ownerProfile} source={{ width: 58, height: 58, uri: 'https://fixthephoto.com/blog/images/uikit_slider/male-photo-edited-by-fixthephoto-service_1649799173.jpg' }}>
+                </Image>
                 {/* details */}
                 <View>
                     {/* name */}
@@ -20,7 +21,70 @@ const Home = () => {
                 </View>
 
             </View>
-            <Text>Home Screen</Text>
+            <View style={styles.HomeSection}>
+
+                <Text style={styles.sectionHeading}>Employees </Text>
+
+                {/* for employees */}
+                <TouchableOpacity activeOpacity={0.6} style={styles.SectionBox}>
+                    {/* employee profile */}
+                    <Image style={styles.ownerProfile} source={{ width: 90, height: 90, uri: 'https://fixthephoto.com/blog/images/uikit_slider/male-photo-edited-by-fixthephoto-service_1649799173.jpg' }}>
+                    </Image>
+                    <View style={styles.EmployeeDetails}>
+                        <Text style={styles.employeeName}>
+                            Employee Name
+                        </Text>
+                        <Text style={styles.atPlace}>
+                            At. Address
+                        </Text>
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity activeOpacity={0.6} style={styles.SectionBox}>
+                    {/* employee profile */}
+                    <Image style={styles.ownerProfile} source={{ width: 90, height: 90, uri: 'https://fixthephoto.com/blog/images/uikit_slider/male-photo-edited-by-fixthephoto-service_1649799173.jpg' }}>
+                    </Image>
+                    <View style={styles.EmployeeDetails}>
+                        <Text style={styles.employeeName}>
+                            Employee Name
+                        </Text>
+                        <Text style={styles.atPlace}>
+                            At. Address
+                        </Text>
+                    </View>
+                </TouchableOpacity>
+                <Text style={styles.sectionHeading}>Sites  </Text>
+
+                {/* for sites */}
+                <TouchableOpacity activeOpacity={0.6} style={styles.SectionBox}>
+                    {/* sites  */}
+                    <View style={styles.EmployeeDetails}>
+                        <Text style={styles.employeeName}>
+                            Client Name
+                        </Text>
+                        <Text style={styles.atPlace}>
+                            At. Address
+                        </Text>
+                        <View style={[componentStyles.badge, { backgroundColor: palette.blue }]}>
+                            <Text style={[componentStyles.badgeText, { color: palette.blueTextColor }]}>Done</Text>
+                        </View>
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity activeOpacity={0.6} style={styles.SectionBox}>
+
+                    <View style={styles.EmployeeDetails}>
+                        <Text style={styles.employeeName}>
+                            Client Name
+                        </Text>
+                        <Text style={styles.atPlace}>
+                            At. Address
+                        </Text>
+                        <View style={[componentStyles.badge, { backgroundColor: palette.success }]}>
+                            <Text style={[componentStyles.badgeText, { color: palette.blueTextColor }]}>In Progress</Text>
+                        </View>
+
+                    </View>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 
@@ -42,19 +106,68 @@ const styles = StyleSheet.create({
         backgroundColor: palette.primary,
         padding: design.paddingSize,
         flexDirection: "row",
-        alignItems:"center"
+        alignItems: "center"
     },
     ownerProfile: {
         resizeMode: "contain",
-        borderRadius: 11,
-        marginRight:10
+        borderRadius: design.borderRadiusProfile,
+        marginRight: 10
     },
-    userName:{
-        fontSize:20,
-        fontFamily:"Inter_600SemiBold"
+    userName: {
+        fontSize: 20,
+        fontFamily: "Inter_600SemiBold"
     },
-    userDesignation:{
-        fontFamily:"Inter_300Light",
-        fontSize:15
+    userDesignation: {
+        fontFamily: "Inter_300Light",
+        fontSize: 15
+    },
+    HomeSection: {
+        padding: design.paddingSize
+
+    },
+    sectionHeading: {
+        fontSize: 18,
+        fontFamily: "Inter_400Regular",
+        marginVertical: 10
+
+    },
+    SectionBox: {
+        backgroundColor: palette.primary,
+        borderWidth: 1,
+        borderColor: palette.borderColor,
+        width: "100%",
+        borderRadius: design.borderRadiusProfile,
+        minHeight: 100,
+        padding: design.paddingSize,
+        flexDirection: "row",
+        marginBottom: 10
+    },
+    EmployeeDetails: {
+    },
+    employeeName: {
+        fontSize: 20,
+        fontFamily: "Inter_500Medium"
+    },
+    atPlace: {
+        fontSize: 15
+        ,
+        fontFamily: "Inter_400Regular"
+    }
+})
+
+const componentStyles = StyleSheet.create({
+    badge: {
+        padding: 3,
+        borderRadius: 9,
+        alignItems: "center",
+
+
+
+    },
+    badgeText: {
+        fontSize: 15,
+        fontFamily: "Inter_400Regular",
+        color: palette.successTextColor,
+
     }
 })
