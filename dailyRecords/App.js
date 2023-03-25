872ed86a0539c36b946eb7306a4e6da9e5934edd
tonyCoder <127786984+tonyCoder08@users.noreply.github.com>
2023-03-25 07:10:49 +0000

    intial setup

---
+++ dailyRecords/App.js
@@ -0,0 +1,9 @@
+import { NavigationContainer } from '@react-navigation/native';
+import StackNavigation from './navigation';
+export default function App() {
+  return (
+    <NavigationContainer>
+      <StackNavigation />
+    </NavigationContainer>
+  );
+}
---
+++ dailyRecords/constants/index.js
@@ -0,0 +1,12 @@
+const palette = {
+    primary:"#fff",
+    secondary:"#F8F9FA",
+    textColor:"#11181C",
+    borderColor:"#D7DBDF",
+    success:"#70FF98",
+    successTextColor:"#419E5B"
+}
+
+const design = {
+    borderRadius:11,
+}
