f6f1103fbdcaa44bffe6929f4ea97654e71d26ab
maddyCoder <maddyyyyyyy6@gmail.com>
2023-03-31 16:41:05 +0000

    Settings page dynamic data

--- dailyRecords/screens/Settings.js
+++ dailyRecords/screens/Settings.js
@@ -1,13 +1,24 @@
 
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import Header from '../components/Header';
+import { settings } from '../constants/settings';
 import { globalstyles } from '../constants/styles'
 
-const Settings = ({navigation}) => {
+const Settings = ({ navigation }) => {
     return (
         <View style={globalstyles.container}>
             <Header title="Settings" navigation={navigation} />
-            
+            <View style={globalstyles.Section} >
+                {
+                    settings.map(tab => <TouchableOpacity style={[globalstyles.box, { minHeight: 70 }]}>
+                        <Text style={styles.title}>
+                            {tab.title}
+                        </Text>
+                    </TouchableOpacity>)
+                }
+
+            </View>
+
         </View>
 
     )
@@ -15,5 +26,8 @@ const Settings = ({navigation}) => {
 export default Settings;
 
 const styles = StyleSheet.create({
-
+    title: {
+        fontSize: 21,
+        fontFamily: "Inter_500Medium"
+    },
 })
