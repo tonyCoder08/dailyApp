14a3492c3db70d465da04afcaa46f5f660a5e797
maddyCoder <maddyyyyyyy6@gmail.com>
2023-04-07 11:03:08 +0000

    imagepicker fix

--- dailyRecords/screens/AddEmployee.js
+++ dailyRecords/screens/AddEmployee.js
@@ -29,11 +29,10 @@ const AddEmployee = ({ navigation }) => {
         let result = await ImagePicker.launchImageLibraryAsync({
             mediaTypes: ImagePicker.MediaTypeOptions.All,
             allowsEditing: true,
-            aspect: [4, 3],
+            aspect: [4, 4],
             quality: 1,
         });
 
-        console.log(result);
 
         if (!result.canceled) {
             setImage(result.assets[0].uri);
